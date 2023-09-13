a7c35830e8fed1170beb10a13ab66208c67db816
mryeminthu <dhammaarchive@hotmail.com>
2023-09-13 10:21:59 -0700

    Add Button.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,43 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
+import Button from './Button';
 
 const Calculator = () => {
-  const getButtonStyle = (value) => {
-    const buttonStyles = {
-      0: 'zero',
-      x: 'operator',
-      '-': 'operator',
-      '+': 'operator',
-      '=': 'operator',
-      '÷': 'operator',
-    };
-    return `button ${buttonStyles[value]}`;
+  const [display, setDisplay] = useState('0');
+
+  const handleButtonClick = (value) => {
+    setDisplay(value);
   };
+
+  const buttons = [
+    'AC', '+/-', '%', { label: '÷', className: 'operator' },
+    '7', '8', '9', { label: 'x', className: 'operator' },
+    '4', '5', '6', { label: '-', className: 'operator' },
+    '1', '2', '3', { label: '+', className: 'operator' },
+    { label: '0', className: 'zero' }, '.', { label: '=', className: 'operator' },
+  ];
 
   return (
     <div className="container">
       <div className="wrapper">
-        <div className="screen">0</div>
+        <div className="screen">{display}</div>
         <div className="buttonBox">
-          <button type="button" className={getButtonStyle('AC')}>AC</button>
-          <button type="button" className={getButtonStyle('+/-')}>+/-</button>
-          <button type="button" className={getButtonStyle('%')}>%</button>
-          <button type="button" className={`operator-bg ${getButtonStyle('÷')}`}>÷</button>
-          <button type="button" className={getButtonStyle('7')}>7</button>
-          <button type="button" className={getButtonStyle('8')}>8</button>
-          <button type="button" className={getButtonStyle('9')}>9</button>
-          <button type="button" className={`operator-bg ${getButtonStyle('x')}`}>x</button>
-          <button type="button" className={getButtonStyle('4')}>4</button>
-          <button type="button" className={getButtonStyle('5')}>5</button>
-          <button type="button" className={getButtonStyle('6')}>6</button>
-          <button type="button" className={`operator ${getButtonStyle('-')}`}>-</button>
-          <button type="button" className={getButtonStyle('1')}>1</button>
-          <button type="button" className={getButtonStyle('2')}>2</button>
-          <button type="button" className={getButtonStyle('3')}>3</button>
-          <button type="button" className={`operator-bg ${getButtonStyle('+')}`}>+</button>
-          <button type="button" className={`zero ${getButtonStyle('0')}`}>0</button>
-          <button type="button" className={getButtonStyle('.')}>.</button>
-          <button type="button" className={getButtonStyle('=')}>=</button>
+          {buttons.map((buttonValue) => (
+            <Button
+              key={buttonValue.label || buttonValue}
+              value={buttonValue.label || buttonValue}
+              onClick={handleButtonClick}
+              className={buttonValue.className || ''}
+            />
+          ))}
         </div>
       </div>
     </div>
